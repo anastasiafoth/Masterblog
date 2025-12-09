0919a4934651096fd8b1df11c171b5014e7a542f
anastasiafoth <anastasiafoth9@gmail.com>
2025-12-09 22:12:09 +0100

    feat: Add Flask blog with JSON data source
    - Create basic Flask application with index route
    - Add template for displaying blog posts
    - Implement JSON file reading for blog posts
    - Add basic styling with CSS
    - Set up project structure

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
+
+
+BLOG_POSTS_FILE = 'data/blog_posts.json'
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    #Reading blog posts from json file
+    try:
+       with open(BLOG_POSTS_FILE, 'r') as fileobj:
+           blog_posts = json.load(fileobj)
+
+    except FileNotFoundError:
+        blog_posts = []
+        print("Error loading blog posts")
+
+    #generating html based on template
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
